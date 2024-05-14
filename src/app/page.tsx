e3f7a4df41a58a709ceb057d99ef1f22c4a5a382
Jesse Lee <jesselee@Jesses-MacBook-Air.local>
2024-05-14 14:12:51 -0700

    database setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
-import Link from "next/link";
+import { db } from "~/server/db";
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+
+  console.log("bruh", posts);
+
   const mockUrls = [
     "https://utfs.io/f/dd238c5b-9c81-4ca7-a55c-c1c9bfde08eb-umtxnt.jpeg",
     "https://utfs.io/f/44fadc8b-716a-4a4b-a1eb-9bb144af9d67-wfz57m.png",
@@ -13,8 +17,8 @@ export default function HomePage() {
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
-          <div key={image.id} className="w-48">
+        {[...mockImages, ...mockImages, ...mockImages].map((image, index) => (
+          <div key={image.id + "-" + index} className="w-48">
             <img src={image.url} />
           </div>
         ))}
