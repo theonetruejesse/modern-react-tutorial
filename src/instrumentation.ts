430e2c7a920ecb2caba143a8dbf3e234740ee0a1
Jesse Lee <jesselee@Jesses-MacBook-Air.local>
2024-05-14 18:23:29 -0700

    implemented sentry

---
+++ src/instrumentation.ts
@@ -0,0 +1,30 @@
+import * as Sentry from "@sentry/nextjs";
+
+export async function register() {
+  if (process.env.NEXT_RUNTIME === "nodejs") {
+    Sentry.init({
+      dsn: "https://48ed2b595fbdb6ebd9a2810f140fc1c3@o4507257705660416.ingest.us.sentry.io/4507257709330432",
+
+      // Adjust this value in production, or use tracesSampler for greater control
+      tracesSampleRate: 1,
+
+      // Setting this option to true will print useful information to the console while you're setting up Sentry.
+      debug: false,
+
+      // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
+      // spotlight: process.env.NODE_ENV === 'development',
+    });
+  }
+
+  if (process.env.NEXT_RUNTIME === "edge") {
+    Sentry.init({
+      dsn: "https://48ed2b595fbdb6ebd9a2810f140fc1c3@o4507257705660416.ingest.us.sentry.io/4507257709330432",
+
+      // Adjust this value in production, or use tracesSampler for greater control
+      tracesSampleRate: 1,
+
+      // Setting this option to true will print useful information to the console while you're setting up Sentry.
+      debug: false,
+    });
+  }
+}
